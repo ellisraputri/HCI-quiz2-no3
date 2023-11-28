8fa5e78191dcfa6e0c2bedfd59321081019afc30
ellisraputri <ellisraputri@gmail.com>
2023-11-28 15:16:22 +0700

    sss

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
-    $('.content').hide()
     $('#submit').click(function(){
         var name = $('#name').val()
         var clas = $('#class').val()
         var country = $('#country').val()
         var color = $('#color').val()
 
-        if (color == 'blue'){
-            $('.name').css('color','blue')
-            $('.clas').css('color','blue')
-            $('.country').css('color','blue')
-            $('.content').show()
+        if (color === 'blue'){
+            $('.content p').css('color','blue')
+            $('.name').text('Name: '+name)
+            $('.clas').text('Class: '+clas)
+            $('.country').text('Country: '+country)
         }
-        else if (color == 'pink'){
-            $('.name').css('color','red')
-            $('.clas').css('color','red')
-            $('.country').css('color','red')
-            $('.content').show()
+        else if (color === 'pink'){
+            $('.content p').css('color','red')
+            $('.name').text('Name: '+name)
+            $('.clas').text('Class: '+clas)
+            $('.country').text('Country: '+country)
         }
     });
 
